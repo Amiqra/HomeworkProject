ad686dcbcdb408d798034b21611b1dd4e34baa5d
Sami <samikale90@gmail.com>
2020-10-12 15:30:21 +0100

    new line added

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -3,5 +3,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("Homework Poroject");
         System.out.println("I created one branch");
+        System.out.println("I added branch");
     }
 }
